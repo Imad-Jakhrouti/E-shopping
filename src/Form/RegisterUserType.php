6dbc4c8e257e61c1cfb64a422ec276d5cbdd5df3
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-07-27 21:15:36 +0100

    Add password confirmation and hashing to user registration - Implement a repeated password field using the RepeatedType - Set the first_options to include the label, placeholder, and hash_property_path for the main password field - Set the second_options to include the label and placeholder for the confirmation password field - Mark the 'plainPassword' field as unmapped ('mapped' => false) to prevent it from being directly persisted to the database - The hashed password will be stored in the 'password' property of the User entity

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -22,11 +23,22 @@ class RegisterUserType extends AbstractType
                     'placeholder' => 'Imadjakhrouti@gmail.com',
                 ]
             ])
-            ->add('password', PasswordType::class,[
-                'label' => 'Mot de passe',
-                'attr' => [
-                    'placeholder' => '**********',
-                ]
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options'  => [
+                    'label' => 'Mot de passe',
+                    'attr' =>[
+                      'placeholder' => 'Taper votre Mot de passe',
+                    ],
+                    'hash_property_path' => 'password',
+                ],
+                'second_options' => [
+                    'label' => 'Confirmation mot de passe',
+                    'attr' =>[
+                        'placeholder' => 'Taper votre Mot de passe',
+                    ],
+                ],
+                'mapped' => false,
             ])
 
             ->add('firstname',TextType::class,[
