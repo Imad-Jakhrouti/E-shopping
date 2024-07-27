35f169732dc30f5dfd6242464099dc88b741ed05
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-07-27 20:58:21 +0100

    Implement registration form fields - Add email, password, firstname, and lastname fields to the registration form - Set appropriate form field types (EmailType, PasswordType, TextType) - Add placeholder attributes for each field to enhance user experience - Add a submit button with the label "Inscrire" and a success button class

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +16,36 @@ class RegisterUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
+            ->add('email',EmailType::class,[
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'Imadjakhrouti@gmail.com',
+                ]
+            ])
+            ->add('password', PasswordType::class,[
+                'label' => 'Mot de passe',
+                'attr' => [
+                    'placeholder' => '**********',
+                ]
+            ])
+
+            ->add('firstname',TextType::class,[
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Imad',
+                ]
+            ])
+            ->add('lastname',TextType::class,[
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Jakhrouti',
+                ]
+            ])
+            ->add('Inscrire', SubmitType::class,[
+                'attr' =>[
+                    'class' => 'btn btn-success'
+                ]
+            ])
         ;
     }
 
