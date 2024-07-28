963e582afdc13ad3ed506b7c7a42bc524b215d6d
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-07-28 23:38:55 +0100

    Enable FormLoginAuthenticator with form_login setting
    - Configured the form_login setting in security.yaml
    - Set the login_path and check_path to the 'app_login' route

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,12 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
     #[Route('/connexion', name: 'app_login')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('login/index.html.twig');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('login/index.html.twig' ,[
+            'error' => $error,
+            'last_username' => $lastUsername,
+        ]);
     }
 }
