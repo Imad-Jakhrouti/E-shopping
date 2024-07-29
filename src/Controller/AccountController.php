29882bab0a8fe9afa4786a8f59d010388d8938e7
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-07-29 21:33:19 +0100

    Implement password change functionality - Inject the UserPasswordHasherInterface into the modifyPassword() method - Create the ModifyPasswordUserType form with the current user as the data object - Pass the hasher service as an option to the form - Call handleRequest() to process the submitted form

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Form\ModifyPasswordUserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AccountController extends AbstractController
@@ -15,9 +18,13 @@ class AccountController extends AbstractController
         return $this->render('account/index.html.twig');
     }
     #[Route('/modifier_mot_de_passe', name: 'app_account_modify_password')]
-    public function modifyPassword(): Response
+    public function modifyPassword(Request $request , UserPasswordHasherInterface $hasher, ): Response
     {
-        $form = $this->createForm(ModifyPasswordUserType::class);
+        $user = $this->getUser();
+        $form = $this->createForm(ModifyPasswordUserType::class , $user , [
+            'hasher' => $hasher,
+        ]);
+        $form->handleRequest($request);
 
         return $this->render('account/modifyPassword.html.twig',[
             'modifyPasswordForm' => $form->createView(),
