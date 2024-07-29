d2f68f2fe6e9d25315faf2b82c183be5c4d289fd
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-07-29 21:20:53 +0100

    Add fields for changing password - Add 'actualPassword' field for current password - Add 'plainPassword' field with repeated password type for new password - Add 'Submit' button with 'Mettre à jour' label

--- src/Form/ModifyPasswordUserType.php
+++ src/Form/ModifyPasswordUserType.php
@@ -4,8 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class ModifyPasswordUserType extends AbstractType
 {
@@ -13,7 +17,47 @@ class ModifyPasswordUserType extends AbstractType
     {
         $builder
 
-            ->add('password')
+            ->add('actualPassword', PasswordType::class, [
+//                'label' => 'Mot de passe actuel',
+                'label_attr' => ['class' => 'd-none'],
+                'attr' => [
+                    'placeholder' => 'Taper votre Mot de passe actuel',
+                    'class' => 'my_input'
+                ],
+
+                'mapped' => false,
+
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+
+                'first_options'  => [
+//                    'label' => 'Mot de passe',
+                    'label_attr' => ['class' => 'd-none'],
+                    'hash_property_path' => 'password',
+                    'attr' => [
+                        'placeholder' => 'Taper votre nouveau Mot de passe',
+                        'class' => 'my_input'
+                    ]
+
+                ],
+                'second_options' => [
+//                    'label' => 'Confirmer mot de passe',
+                    'label_attr' => ['class' => 'd-none'],
+                    'attr' => [
+                        'placeholder' => 'Confirmer votre nouveau mot de passe',
+                        'class' => 'my_input'
+
+                    ]
+                ],
+                'mapped' => false,
+            ])
+            ->add('Submit',SubmitType::class , [
+                'label' => 'Mettre a jour',
+                'attr' => [
+                    'class' => 'my_button btn'
+                ]
+            ])
 
         ;
     }
