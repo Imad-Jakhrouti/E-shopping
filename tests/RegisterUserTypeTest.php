cdafa9ab12c283699ee4dfb2cb30d55d8366820b
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-07-30 23:09:27 +0100

    Verify successful registration and redirect to login page
    - Create a functional test to simulate the user registration process
    - Use the test client to submit the registration form with valid data
    - Verify that the user is redirected to the login page after successful registration
    - Ensure that the "Inscription reussite veuillez vous connecter!" flash message is displayed on the login page
    - This test ensures that the desired user flow and feedback is correctly implemented in the application
    - The test sets up the necessary test environment by creating the test database and running migrations
    - The test data is isolated within the test environment to avoid interfering with the development or production databases

--- tests/RegisterUserTypeTest.php
+++ tests/RegisterUserTypeTest.php
@@ -8,10 +8,30 @@ class RegisterUserTypeTest extends WebTestCase
 {
     public function testSomething(): void
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        // 1. Cree un faux client (navigateur) de pointer vers une URL(jouer une URL)
+        // 2. Remplire les champs de mon formulaire d'inscription
+        // 3. Est-ce-que tu peut regarder si dans mon page j'ai le message d'alert suivant: "Inscription reussite veuillez vous connecter!"
 
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+
+        // Cree un faux client (navigateur)
+        $client = static::createClient();
+        // Pointer vers une URL(jouer une URL)
+        $client->request('GET' ,'/inscription');
+        // 2. Remplire les champs de mon formulaire d'inscription
+        $client->submitForm('Valider votre formulaire',[
+            'register_user[email]' => 'test@test.com',
+            'register_user[plainPassword][first]' => 'password',
+            'register_user[plainPassword][second]' => 'password',
+            'register_user[firstname]' => 'Imad',
+            'register_user[lastname]' => 'Test',
+        ]);
+
+        // assurer la redirection
+        $this->assertResponseRedirects('/connexion');
+        // make the client follow the redirection
+        $client->followRedirect();
+
+        // 3. Est-ce-que tu peut regarder si dans mon page j'ai le message d'alert suivant: "Inscription reussite veuillez vous connecter!"
+        $this->assertSelectorExists('div:contains("Inscription reussite veuillez vous connecter!")');
     }
 }
