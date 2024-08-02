25ad566e9e22b61208e22be8b9f66a2703a8c3ac
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-08-02 16:21:25 +0100

    add product CRUD link to the dashboard's menu

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use App\Entity\Product;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -45,6 +46,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class);
         yield MenuItem::linkToCrud('Catégories', 'fas fa-list-alt', Category::class);
-
+        yield MenuItem::linkToCrud('Produits', 'fas fa-shopping-bag', Product::class);
     }
 }
