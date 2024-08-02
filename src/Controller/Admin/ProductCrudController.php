28e89f9a37426fadd3de365470cf8487fbbdb111
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-08-02 16:25:40 +0100

    customize CRUD labels - Add configureCrud() method in ProductController - Set the singular label to 'Produit' - Set the plural label to 'Produits' - Improve the user experience by using more appropriate and localized labels - Enhance the consistency of the admin interface for product management

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -14,6 +15,14 @@ class ProductCrudController extends AbstractCrudController
     {
         return Product::class;
     }
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+
+            ->setEntityLabelInSingular('Produit')
+            ->setEntityLabelInPlural('Produits');
+    }
+
 
     /*
     public function configureFields(string $pageName): iterable
