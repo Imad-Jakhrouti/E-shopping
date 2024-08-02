1d22386cb40c4cc09fd674e64153dee5c311c407
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-08-02 16:36:12 +0100

    configure CRUD fields - Add configureFields() method in ProductController - Define the fields to be displayed in the CRUD interface for products - Add fields for name, slug, description, image, price, and tax rate - Leverage the EasyAdmin field types for enhanced customization and user experience - Provide helpful hints and descriptions for each field - Enable associations between products and categories - Improve the overall structure and clarity of the product management interface

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -5,7 +5,12 @@ namespace App\Controller\Admin;
 use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -24,14 +29,31 @@ class ProductCrudController extends AbstractCrudController
     }
 
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name', 'Titre')
+                ->setHelp('Le titre de produit')
+                ->setRequired(true),
+            SlugField::new('slug', 'URL')
+                ->setTargetFieldName('name')->
+                setHelp('URL  de votre produit'),
+            TextEditorField::new('description', 'Description')
+                ->setHelp('Description de votre produit'),
+            ImageField::new('image', 'Image')
+                ->setHelp('Image de votre produit')
+                ->setUploadDir('public/uploads'),
+            NumberField::new('price', 'Prix')
+                ->setHelp('Le prix de votre produit'),
+            ChoiceField::new('tva','TVA')
+                ->setHelp('TVA de votre produit')
+                ->setChoices([
+                    '5.5%' => '5.5',
+                    '10%' => '10',
+                    '15%' => '15',
+                    '20%' => '20',
+                ]),
+            AssociationField::new('category', 'Catégorie associé')
         ];
     }
-    */
 }
