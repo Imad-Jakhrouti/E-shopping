b68c7e3b8f2ae283b5e2bc970132d3d493b3b394
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-08-02 17:59:34 +0100

    enhance category page display -Update the Twig template to show the category name and product count - Implement a responsive grid layout for the product listings - Apply custom styling to the product items for a consistent visual appearance - Add hover effects on the product images to enhance the user experience - Ensure the page layout and styling are visually appealing and user-friendly

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class CategoryController extends AbstractController
 {
-    #[Route('/categories/{slug}', name: 'app_category')]
+    #[Route('/categorie/{slug}', name: 'app_category')]
     public function index($slug ,CategoryRepository $categoryRepository): Response
     {
         $category = $categoryRepository->findOneBy(['slug' => $slug]);
