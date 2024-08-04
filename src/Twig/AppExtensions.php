379e56acf4b32bbec7a8e09f191cb7ae9d12bd3f
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-08-04 23:26:47 +0100

    Expose all categories as a global Twig variable

--- src/Twig/AppExtensions.php
+++ src/Twig/AppExtensions.php
@@ -1,12 +1,18 @@
 <?php
-
 namespace App\Twig;
 
+use App\Class\Cart;
+use App\Repository\CategoryRepository;
 use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
 use Twig\TwigFilter;
 
-class AppExtensions extends AbstractExtension
-{
+class AppExtensions extends AbstractExtension implements GlobalsInterface {
+    private $categoryRepository ;
+
+    public function __construct(CategoryRepository $categoryRepository ) {
+        $this->categoryRepository = $categoryRepository;
+    }
     public function getFilters(): array{
         return [
             new TwigFilter('formatPrice', [$this, 'formatPrice']),
@@ -15,5 +21,12 @@ class AppExtensions extends AbstractExtension
 
     public function formatPrice($price){
         return number_format($price, 0, '', ' '). 'DH' ;
+    }
+
+    public function getGlobals(): array
+    {
+        return [
+            'allCategory' => $this->categoryRepository->findAll(),
+        ];
     }
 }
