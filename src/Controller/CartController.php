36f8093ff5470cf2e6cab553d733528b7a5ee004
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-08-06 23:03:54 +0100

    add product to cart
    Implemented the `add` action in the CartController to allow users to add a product to their cart.
    
    The action takes the product ID as a parameter, uses the ProductRepository to retrieve the corresponding product, and then adds it to the user's cart using the Cart service.

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Class\Cart;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,5 +17,13 @@ class CartController extends AbstractController
         return $this->render('cart/index.html.twig', [
             'controller_name' => 'CartController',
         ]);
+    }
+
+    #[Route('/cart/add/{id}', name: 'app_cart_add')]
+    public function add($id, ProductRepository $productRepository , Cart $cart,Request $request): Response
+    {
+        $product = $productRepository->findOneBy(['id' => $id]);
+        $cart->addToCart($product);
+        return $this->redirect($request->headers->get('referer'));
     }
 }
