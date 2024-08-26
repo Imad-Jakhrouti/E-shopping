18b18d7bea4b0088ffb97a5128f6b573ce70b8dd
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-08-26 12:28:41 +0100

    Add fields to configure the Carrier entity in the admin interface

--- src/Controller/Admin/CarrierCrudController.php
+++ src/Controller/Admin/CarrierCrudController.php
@@ -6,6 +6,8 @@ use App\Entity\Carrier;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -24,14 +26,14 @@ class CarrierCrudController extends AbstractCrudController
             ->setEntityLabelInPlural('Transporteurs');
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name', 'Nom du transporteur'),
+            TextareaField::new('description', 'Description du transporteur'),
+            NumberField::new('price', 'Prix T.T.C du transporteur'),
         ];
     }
-    */
+
 }
